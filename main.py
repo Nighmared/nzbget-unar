f943db1de6b7a3f906cd79dd66b440e237c05df4
nighmared <jonas.konrath@icloud.com>
2025-01-01 22:32:37 +0100

    updated manifest and completed implementation

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
 import os
+import subprocess
 import sys
 
 POSTPROCESS_SUCCESS = 93
+POSTPROCESS_ERROR = 94
+POSTPROCESS_NONE = 95
 
-for a, b in os.environ.items():
-    print("[INFO] ", str(a).ljust(30), "=", b)
+DIR_KEY = "NZBPP_DIRECTORY"
+NZB_NAME_KEY = "NZBPP_NZBNAME"
+NZB_FILE_KEY = "NZBPP_NZBFILENAME"
 
 
-sys.exit(POSTPROCESS_SUCCESS)
+def iprint(*args, **kwargs):
+    print("[INFO]", *args, **kwargs)
+
+
+def eprint(*args, **kwargs):
+    print("[ERROR]", *args, **kwargs)
+
+
+def main():
+
+    try:
+        dest_dir = os.environ[DIR_KEY]
+        dest_dir_found = True
+    except KeyError:
+        dest_dir = "dest dir not found"
+        dest_dir_found = False
+
+    if not dest_dir_found:
+        iprint("exiting with none (??)")
+        sys.exit(POSTPROCESS_NONE)
+
+    parsed_path = os.path.normpath(dest_dir)
+    listing = os.listdir(parsed_path)
+    if len(listing) == 1:
+        new_path = os.path.join(parsed_path, listing[0])
+        if os.path.isdir(new_path):
+            parsed_path = new_path
+            listing = os.listdir(parsed_path)
+
+    if len(listing) < 5:
+        iprint("Skipping, does not need unar")
+        sys.exit(POSTPROCESS_NONE)
+
+    num_to_part_rar_paths: dict[int, str] = {}
+    for f in os.listdir(parsed_path):
+        if f[-3] == "r" and (num := f[-2:]).isdigit():
+            key = int(num)
+            val = os.path.join(parsed_path, f)
+            num_to_part_rar_paths[key] = val
+
+    a = sorted(num_to_part_rar_paths.items(), key=lambda x: x[0])
+    cmd = f"unrar x {a[0][1]} {parsed_path}"
+    retc = subprocess.call(cmd, shell=True)
+    if retc != 0:
+        eprint(f"Unrar exited with code {retc}")
+        sys.exit(POSTPROCESS_ERROR)
+    iprint("Successfully extracted from multi-archive")
+    sys.exit(POSTPROCESS_SUCCESS)
+
+
+if __name__ == "__main__":
+    main()
