8c0dd821e3acc04d59770e9bd9a494c9a43071ce
nighmared <jonas.konrath@icloud.com>
2025-01-01 21:33:41 +0100

    aa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 POSTPROCESS_SUCCESS = 93
 
 for a, b in os.environ.items():
-    print(str(a).ljust(30), "=", b)
+    print("[INFO] ", str(a).ljust(30), "=", b)
 
 
 sys.exit(POSTPROCESS_SUCCESS)
